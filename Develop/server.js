fc4ecf8ada4d3034a8c09aa173a00109bfba4a4f
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-09 23:57:22 -0400

    deploying45

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 
 const app = express();
 
