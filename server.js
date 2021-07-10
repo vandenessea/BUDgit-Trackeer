3b3be9a07f4583ffdf0cbcce0c0eea58861451ac
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-10 09:58:43 -0400

    fixed?

--- server.js
+++ server.js
@@ -16,13 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/BudgetDB',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
+process.env.MONGODB_URI || 'mongodb://localhost/BudgetDB'
+  
 );
 
 // routes
